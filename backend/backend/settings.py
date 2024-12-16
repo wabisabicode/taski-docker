100905de758d070725246511a776a81792c6d6dd
wabisabicode <tretyakov@uni-mainz.de>
2024-12-16 17:12:43 +0100

    fix ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['51.250.31.82', 'polarstern13.de']
 
 
 # Application definition
